0d4ba4b779e53b471afe84ac93d288881a5d7d8c
Abhi256207 <amani@mail.bradley.edu>
2017-04-22 20:00:01 -0500

    url change

--- https_example.py
+++ https_example.py
@@ -7,12 +7,12 @@ context.check_hostname = True
 context.load_default_certs()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_sock = context.wrap_socket(s, server_hostname='https://github.com/Abhi256207/ECEx73hw4-1')
+ssl_sock = context.wrap_socket(s, server_hostname='help.github.com')
 
 try:
-    ssl_sock.connect(('https://github.com/Abhi256207/ECEx73hw4-1', 443))
+    ssl_sock.connect(('help.github.com', 443))
     ssl_sock.settimeout(1.0)
-    ssl_sock.sendall("GET /articles/which-remote-url-should-i-use/ HTTP/1.1\r\nHostname: https://github.com/Abhi256207/ECEx73hw4-1\r\n\r\n")
+    ssl_sock.sendall("GET /articles/which-remote-url-should-i-use/ HTTP/1.1\r\nHostname:help.github.com\r\n\r\n")
     while 1:
         try:
             data = ssl_sock.recv(2048).strip()
